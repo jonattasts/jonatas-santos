41a7e2691bd6246718a10d4170732c672f103728
Jonatas Santos <jonatas.santos@produs.dom>
2021-02-25 13:24:44 -0300

    Alterado o css dos botões de filtro da seção 'Portifólio'

--- js/main.js
+++ js/main.js
@@ -179,8 +179,8 @@
     layoutMode: 'fitRows'
   });
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
+  $('#portfolio-filters li').on( 'click', function() {
+    $("#portfolio-filters li").removeClass('filter-active');
     $(this).addClass('filter-active');
 
     portfolioIsotope.isotope({ filter: $(this).data('filter') });
