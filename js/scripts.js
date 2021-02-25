a57de7db3a2444998c1089c984c510cf080d3f3e
Jonatas Santos <jonatas.santos@produs.dom>
2021-02-24 23:34:11 -0300

    Corrigido bug ao clicar nos cards da seção 'Portifólio' na visão mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
 //Bloqueia o click nos icones ocultos dos cards da seção Portifólio
+$(".link-option").on("click", function (event) {
+    if ($(this).css("opacity") == 0) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+});
